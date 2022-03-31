99b8410433cfddc2a0b847582eac79e312f0f1bd
Talia Blain <tb1126@york.ac.uk>
2022-03-31 20:57:42 +0100

    -

--- dbScript.py
+++ dbScript.py
@@ -7,14 +7,14 @@ from sqlalchemy import text
 engine = create_engine("mysql+pymysql://root:@localhost/Exercise18", echo=True, future=True)
 
 with engine.connect() as conn:
-    print("Hello")
-    conn.execute(text("CREATE TABLE some_table2 (x int, y int)"))
-    conn.execute(text("INSERT INTO some_table2 (x, y) VALUES (:x, :y)")
+    #print("Hello")
+    conn.execute(text("CREATE TABLE some_table3 (x int, y int)"))
+    conn.execute(text("INSERT INTO some_table3 (x, y) VALUES (:x, :y)")
         [{"x": 1, "y": 1}, {"x": 2, "y":4}]
     )
     conn.commit()
 
 with engine.connect() as conn:
-    result = conn.execute(text("SELECT x, y FROM some_table2"))
+    result = conn.execute(text("SELECT x, y FROM some_table3"))
     for row in result:
         print(f"x: {row.x} y: {row.y}")
