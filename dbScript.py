8c2d28ef89ffaf89fc9ab26eded32849c0ae0280
Talia Blain <tb1126@york.ac.uk>
2022-03-31 20:53:57 +0100

    Initial commit

---
+++ dbScript.py
@@ -0,0 +1,20 @@
+import sqlalchemy
+
+#print(sqlalchemy.__version__)
+
+from sqlalchemy import create_engine
+from sqlalchemy import text
+engine = create_engine("mysql+pymysql://root:@localhost/Exercise18", echo=True, future=True)
+
+with engine.connect() as conn:
+    print("Hello")
+    conn.execute(text("CREATE TABLE some_table2 (x int, y int)"))
+    conn.execute(text("INSERT INTO some_table2 (x, y) VALUES (:x, :y)")
+        [{"x": 1, "y": 1}, {"x": 2, "y":4}]
+    )
+    conn.commit()
+
+with engine.connect() as conn:
+    result = conn.execute(text("SELECT x, y FROM some_table2"))
+    for row in result:
+        print(f"x: {row.x} y: {row.y}")
