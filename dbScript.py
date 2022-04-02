f0af72655fe7f0371c9e046e262adeb8360ca844
Talia Blain <tb1126@york.ac.uk>
2022-04-02 17:09:55 +0100

    -

--- dbScript.py
+++ dbScript.py
@@ -6,15 +6,13 @@ from sqlalchemy import create_engine
 from sqlalchemy import text
 engine = create_engine("mysql+pymysql://root:@localhost/Exercise18", echo=True, future=True)
 
-with engine.connect() as conn:
-    #print("Hello")
-    conn.execute(text("CREATE TABLE some_table3 (x int, y int)"))
-    conn.execute(text("INSERT INTO some_table3 (x, y) VALUES (:x, :y)")
-        [{"x": 1, "y": 1}, {"x": 2, "y":4}]
-    )
-    conn.commit()
+genre = input("Which book would you like?")
 
 with engine.connect() as conn:
-    result = conn.execute(text("SELECT x, y FROM some_table3"))
+    result = conn.execute(
+        text("select BOOK_ID, AUTHUR, TITLE, GENRE from BOOK_INFO"),
+        {"ID": genre}
+    )
     for row in result:
-        print(f"x: {row.x} y: {row.y}")
+        print(f"AUTHOR: {row.AUTHUR}")
+
